70265d65bf6d15b8dd909502ed27c1ce740e80f9
Maria Tonnesen <maria.tonnesen@thetrainline.com>
2022-12-02 20:04:55 +0000

    Day 1 Part 1

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿
+const string fileName = "Inputs/Day1.txt";
+
+var puzzleInput = File.ReadAllLines(fileName);
+var largestCalorie = 0;
+var currentElfCalories = 0;
+
+foreach (var line in puzzleInput)
+{
+    if (int.TryParse(line, out var calories))
+    {
+        currentElfCalories += calories;
+    }
+    else
+    {
+        if (currentElfCalories > largestCalorie)
+        {
+            largestCalorie = currentElfCalories;
+        }
+        currentElfCalories = 0;
+    }
+}
+if (currentElfCalories > largestCalorie)
+{
+    largestCalorie = currentElfCalories;
+}
+
+Console.WriteLine(largestCalorie);
