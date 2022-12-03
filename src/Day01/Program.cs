129fc5d4fecad6434880ef0fc235ae6559d620ea
Maria Tonnesen <maria.tonnesen@thetrainline.com>
2022-12-03 15:37:49 +0000

    Day 1 Part 2

--- src/Day01/Program.cs
+++ src/Day01/Program.cs
@@ -1,7 +1,10 @@
-﻿
+﻿using System.Linq;
+
 const string fileName = "input.txt";
 
 var puzzleInput = File.ReadAllLines(fileName);
+
+// PART 1
 var largestCalorie = 0;
 var currentElfCalories = 0;
 
@@ -27,3 +30,28 @@ if (currentElfCalories > largestCalorie)
 }
 
 Console.WriteLine(largestCalorie);
+
+// PART 2
+var caloriesCounted = new List<int>();
+var currentCalories = 0;
+
+foreach (var line in puzzleInput)
+{
+    if (int.TryParse(line, out var calories))
+    {
+        currentCalories += calories;
+    }
+    else
+    {
+        caloriesCounted.Add(currentCalories);
+        currentCalories = 0;
+    }
+}
+caloriesCounted.Add(currentCalories);
+currentCalories = 0;
+
+var caloriesSorted = caloriesCounted.OrderByDescending(c => c).ToArray();
+
+var result = caloriesSorted[0] + caloriesSorted[1] + caloriesSorted[2]; 
+
+Console.WriteLine(result);
