4b9a9bbb16bf915152c62bd412ae202da605b359
Maria Tonnesen <maria.tonnesen@thetrainline.com>
2022-12-04 17:26:06 +0000

    Day 03 Part 2

---
+++ src/Day03/Part2.cs
@@ -0,0 +1,32 @@
+﻿namespace Day03;
+
+public static class Part2
+{
+    public static void Run()
+    {
+        const string fileName = "input.txt";
+        var rucksacks = File.ReadAllLines(fileName);
+
+        var prioritySum = 0;
+        
+        var rucksacksGrouped = rucksacks.Chunk(3);
+
+        foreach (var rucksackGroup in rucksacksGrouped)
+        {
+            var firstElf = rucksackGroup[0];
+            var secondElf = rucksackGroup[1];
+            var thirdElf = rucksackGroup[2];
+
+            var firstIntersect = firstElf.Intersect(secondElf);
+            var commonCharacter = firstIntersect.Intersect(thirdElf).First();
+            
+            var priorityList = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            var letterPriority = priorityList.IndexOf(commonCharacter) +1;
+            
+            prioritySum += letterPriority;
+
+        }
+
+        Console.WriteLine(prioritySum);
+    }
+}
--- src/Day03/Program.cs
+++ src/Day03/Program.cs
@@ -2,4 +2,4 @@
 using Day03;
 
 Part1.Run();
-
+Part2.Run();
