77418bf746cb91b4c5ea1c4c8895190da1159cf4
Maria Tonnesen <maria.tonnesen@thetrainline.com>
2022-12-08 09:37:36 +0000

    WIP Day 07 Part 2

--- src/Day07/Part2.cs
+++ src/Day07/Part2.cs
@@ -1,9 +1,43 @@
-﻿namespace Day07;
+﻿using System.Text.RegularExpressions;
+
+namespace Day07;
 
 public static class Part2
 {
-    public static void Run()
+    public static string Run(string input)
     {
-        
+        var commands = input
+            .Split("$ ", StringSplitOptions.RemoveEmptyEntries)
+            .Select(text =>
+            {
+                var commandLines = text
+                    .ReplaceLineEndings(Environment.NewLine)
+                    .Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries);
+                return new Command(commandLines[0], commandLines.Skip(1).ToList());
+            })
+            .ToArray();
+
+        var rootDirectory = new Directory("/");
+        var directory = rootDirectory;
+
+        foreach (var command in commands.Skip(1))
+        {
+            var cdMatch = Regex.Match(command.Text, @"^cd (.*)");
+            if (cdMatch.Success)
+            {
+                var cdTarget = cdMatch.Groups[1].Value;
+                directory = directory.Cd(cdTarget);
+            }
+            else if (command.Text == "ls")
+                directory.Ls(command.Output);
+        }
+
+        var results = new Dictionary<string, long>();
+        rootDirectory.MeasureSizes(results);
+
+        var orderedResults = results.Values.OrderByDescending(n => n);
+        Console.WriteLine(orderedResults.First());
+
+        return results.Values.Sum().ToString();
     }
 }
